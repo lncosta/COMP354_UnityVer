100ca650ece0fa97f2fe8d1f31b3f3a0c7aaccd1
Whitewolf04 <tominhtuan18@gmail.com>
2022-11-30 13:30:51 -0500

    Successfully get the books_id from database
    WIP: Trying to figure out how to load books onto shelves in Unity

--- Assets/Scripts/LogInPage.cs
+++ Assets/Scripts/LogInPage.cs
@@ -47,6 +47,27 @@ public class LogInPage : MonoBehaviour
 					//open welcom panel
 					WebApp.SetActive (true);
 					Debug.Log("<color=green>"+w.text+"</color>");//user exist
+
+                    // Load the shelves with books from the database
+                    //foreach(Shelf shelf in AppManager.CurrentUser.Data.CustomShelves)
+                    //{
+                    //    if(shelf.type == ShelfType.TOREAD)
+                    //    {
+                    //        Debug.Log("<color=yellow>In TO_READ shelf</color>");
+                    //    } else if(shelf.type == ShelfType.READING)
+                    //    {
+                    //        Debug.Log("<color=yellow>In READING shelf</color>");
+                    //    } else if(shelf.type == ShelfType.READ)
+                    //    {
+                    //        Debug.Log("<color=yellow>In READ shelf</color>");
+                    //    } else if(shelf.type == ShelfType.FAVORITES)
+                    //    {
+                    //        Debug.Log("<color=yellow>In FAVORITES shelf</color>");
+                    //    } else if(shelf.type == ShelfType.RECOMMENDATION)
+                    //    {
+                    //        Debug.Log("<color=yellow>In RECOMMENDATION shelf</color>");
+                    //    }
+                    //}
 				}
 			}
 		}
--- Assets/php/index.php
+++ Assets/php/index.php
@@ -1,10 +1,40 @@
 <?php 
 	include_once("db.php");
 
-	if (isset($_POST["username"]) && !empty($_POST["username"]) && 
-		isset($_POST["password"]) && !empty($_POST["password"])){
+	
+	function getUserData($username, $password){
+		GLOBAL $con;
 
-		Login($_POST["username"], $_POST["password"]);
+		$sql = "SELECT shelf_type, books_id FROM Shelf JOIN User ON User.to_read_shelf = Shelf.shelf_id WHERE username=? AND password=? 
+		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.reading_shelf = Shelf.shelf_id WHERE username=? AND password=? 
+		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.read_shelf = Shelf.shelf_id WHERE username=? AND password=?
+		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.favorite_shelf = Shelf.shelf_id WHERE username=? AND password=?
+		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.recommendation_shelf = Shelf.shelf_id WHERE username=? AND password=?";
+		$st = $con->prepare($sql);
+
+		$st->execute(array($username, sha1($password), $username, sha1($password), $username, sha1($password), $username, sha1($password), $username, sha1($password)));
+		$all = $st->fetchAll();
+
+		if(count($all) > 0){
+			for($shelf = 0; $shelf < count($all); $shelf++){
+				// if(strcmp($all[$shelf]["shelf_type"],"to_read") == 0){
+				// 	echo $all[$shelf]["books_id"].",";
+				// } else if(strcmp($all[$shelf]["shelf_type"],"reading") == 0){
+				// 	echo $all[$shelf]["books_id"].",";
+				// } else if(strcmp($all[$shelf]["shelf_type"],"read") == 0){
+				// 	echo $all[$shelf]["books_id"].",";
+				// } else if(strcmp($all[$shelf]["shelf_type"],"favorite") == 0){
+				// 	echo $all[$shelf]["books_id"].",";
+				// } else {
+				// 	echo $all[$shelf]["books_id"];
+				// }
+				if($shelf == count($all)-1){
+					echo $all[$shelf]["books_id"];	
+				} else{
+					echo $all[$shelf]["books_id"].",";
+				}
+			}
+		}
 	}
 
 	function Login($username, $password){
@@ -21,12 +51,23 @@
 		}
 
 		//if username or password are empty strings
-		echo "SERVER: error, invalid username or password";
+		echo "SERVER: Login successful!";
 		exit();
 	}
 
+
+	if (isset($_POST["username"]) && !empty($_POST["username"]) && 
+		isset($_POST["password"]) && !empty($_POST["password"])){
+
+		getUserData($_POST["username"], $_POST["password"]);
+	}
+
+	// getUserData("johndoe", "johndoe");
+
+	
+
 	//if username or password is null (not set)
-	echo "SERVER: error, enter a valid username & password";
+	// echo "SERVER: Logged in";
 
 	//exit():  means end server connection (don't execute the rest)
 ?>
