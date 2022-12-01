ea9e7147d0c90a3f9d0d868a01041e023efae6d2
Luiza Costa <luiza.n16@gmail.com>
2022-12-01 18:47:00 -0500

    Serialization

--- Assets/php/index.php
+++ Assets/php/index.php
@@ -5,36 +5,39 @@
 	function getUserData($username, $password){
 		GLOBAL $con;
 
-		$sql = "SELECT shelf_type, books_id FROM Shelf JOIN User ON User.to_read_shelf = Shelf.shelf_id WHERE username=? AND password=? 
-		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.reading_shelf = Shelf.shelf_id WHERE username=? AND password=? 
-		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.read_shelf = Shelf.shelf_id WHERE username=? AND password=?
-		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.favorite_shelf = Shelf.shelf_id WHERE username=? AND password=?
-		UNION SELECT shelf_type, books_id FROM Shelf JOIN User ON User.recommendation_shelf = Shelf.shelf_id WHERE username=? AND password=?";
+		$sql = "SELECT userData FROM User WHERE username=? AND password=?";
 		$st = $con->prepare($sql);
 
-		$st->execute(array($username, sha1($password), $username, sha1($password), $username, sha1($password), $username, sha1($password), $username, sha1($password)));
+		$st->execute(array($username, sha1($password)));
 		$all = $st->fetchAll();
 
 		if(count($all) > 0){
-			for($shelf = 0; $shelf < count($all); $shelf++){
-				// if(strcmp($all[$shelf]["shelf_type"],"to_read") == 0){
-				// 	echo $all[$shelf]["books_id"].",";
-				// } else if(strcmp($all[$shelf]["shelf_type"],"reading") == 0){
-				// 	echo $all[$shelf]["books_id"].",";
-				// } else if(strcmp($all[$shelf]["shelf_type"],"read") == 0){
-				// 	echo $all[$shelf]["books_id"].",";
-				// } else if(strcmp($all[$shelf]["shelf_type"],"favorite") == 0){
-				// 	echo $all[$shelf]["books_id"].",";
-				// } else {
-				// 	echo $all[$shelf]["books_id"];
-				// }
-				if($shelf == count($all)-1){
-					echo $all[$shelf]["books_id"];	
-				} else{
-					echo $all[$shelf]["books_id"].",";
-				}
-			}
+			echo $all[0]["userData"];
 		}
+		
+	}
+
+	function pushUserData($data, $username, $password){
+		GLOBAL $con;
+
+		$sql = "UPDATE User SET userData=? WHERE username=? AND password=?";
+		$st = $con->prepare($sql);
+
+		try{
+			$st->execute(array($data, $username, sha1($password)));
+			$all=$st->fetchAll();
+			if (count($all) > 0){
+
+				echo "SERVER: ID#".$all[0]["userData"];
+			}
+			else{
+				echo $st->rowCount() . " userData";
+			}
+			
+		} catch(PDOException $e){
+			echo $sql."<br>".$e->getMessage();
+		}
+		
 	}
 
 	function Login($username, $password){
@@ -46,33 +49,31 @@
 		$st->execute(array($username, sha1($password)));//encrypt password
 		$all=$st->fetchAll();
 		if (count($all) == 1){
-			if($all[0]["username"] == "" && $password == ""){
-				echo "SERVER: error - empty fields!";
-			}
-			else{
-				echo "SERVER: ID#".$all[0]["username"];
-				//getUserData($username,  $password);
-			}
+			echo "SERVER: ID#".$all[0]["username"];
+			echo "SERVER: Login successful!";
 			
-			//exit();
 		}
 		else{
 			//if username or password are empty strings
-			echo "SERVER: error - empty fields!";
-			//exit();
+			
+			echo "error - Duplicate users. Contact admin!";
 		}
 
 		
 	}
 
 
-	if (isset($_POST["username"]) && !empty($_POST["username"]) && 
+	if(isset($_POST["data"]) && !empty($_POST["data"]) && isset($_POST["username"]) && !empty($_POST["username"]) && 
+	isset($_POST["password"]) && !empty($_POST["password"])){
+		
+		pushUserData($_POST["data"], $_POST["username"], $_POST["password"]);
+	} else if (isset($_POST["username"]) && !empty($_POST["username"]) && 
 		isset($_POST["password"]) && !empty($_POST["password"])){
 
-		Login($_POST["username"], $_POST["password"]);
+		getUserData($_POST["username"], $_POST["password"]);
 	}
 	else{
-		echo "SERVER: error - empty fields!";
+		echo "error - Empty fields!";
 	}
 
 	// getUserData("johndoe", "johndoe");
