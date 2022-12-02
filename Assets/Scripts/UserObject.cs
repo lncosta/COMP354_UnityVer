0f2491e7cdabc129642b996846618f52482df90d
French Vivec <69429746+Saavedroo@users.noreply.github.com>
2022-12-02 15:36:19 -0500

    UserObject can now access its UserData shelves

--- Assets/Scripts/UserObject.cs
+++ Assets/Scripts/UserObject.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using Shelf.Genre;
+using System.Linq;
 
 [System.Serializable]
 public class UserObject : MonoBehaviour
@@ -11,24 +11,40 @@ public class UserObject : MonoBehaviour
     private Dictionary<string, int> authorScores; // The score for each author present in the user premade shelves
     private Dictionary<Genre, int> genreScores; // The score for each genre present in the user premade shelves
 
-    public int GetBookScore(BookData book) // This returns a sum of the scores of the book's author and the book's genre
+    // getShelf() Explores the shelves of the user to obtain the requested one
+    private Shelf getShelf(ShelfType shelftype)
     {
-        //string author = book.getAuthor(); // Need a method to recover the author of the book
-        //Genre genre = book.getGenre(); // Need a method to recover the genre of the book
-        string author = string.Empty;
-        Genre genre = 0;
+        List<Shelf> CustomShelves = Data.CustomShelves;
+        foreach (Shelf shelf in CustomShelves)
+        {
+            if(shelf.type == shelftype)
+            {
+                return shelf;
+            }
+        }
+
+        return null;
+    }
+
+    // GetBookScore(): This returns a sum of the scores of the book's author and the book's genre. 0 if the book is in a shelf
+    public int GetBookScore(BookData book)
+    {
+        string author = book.author;
+        Genre genre = book.genre;
+        //string author = string.Empty;
+        //Genre genre = 0;
 
         int aScore;
         int gScore;
 
-        // Need methods to acquire each predefined shelf in a user's data
-        //Shelf toRead = Data.getToReadShelf();
-        //Shelf reading = Data.getToReadShelf();
-        //Shelf read = Data.getToReadShelf();
-        //Shelf favorite = Data.getToReadShelf();
-        Shelf toRead = null, reading = null, read = null, favorite = null;
+        Shelf toRead = getShelf(ShelfType.TOREAD);
+        Shelf reading = getShelf(ShelfType.READING);
+        Shelf read = getShelf(ShelfType.READ);
+        Shelf favorites = getShelf(ShelfType.FAVORITES);
+        //Shelf toRead = null, reading = null, read = null, favorite = null;
 
-        if (toRead.GetBooks().Contains(book) || reading.GetBooks().Contains(book) || read.GetBooks().Contains(book) || favorite.GetBooks().Contains(book)) // Verify if the book is already in a shelf
+        // Verify if the book is already in a shelf
+        if (toRead.GetBooks().Contains(book) || reading.GetBooks().Contains(book) || read.GetBooks().Contains(book) || favorites.GetBooks().Contains(book))
         {
             return 0;
         }
@@ -51,14 +67,14 @@ public class UserObject : MonoBehaviour
 
         return aScore+gScore;
     }
-
-    public void buildScores() // Scans the premade shelves to retrieve the score of the contained authors and genres
+    // buildScores(): Scans the premade shelves to retrieve the score of the contained authors and genres
+    public void buildScores()
     {
         this.authorScores = GetAuthorScores();
         this.genreScores = GetGenreScores();
     }
-
-    private void IncrementScore(ref Dictionary<string, int> dico, string author, int increment) // Increases by "increment" the score of an author/genre if it exists, sets it to increment if it doesn't
+    // IncrementScore(): Increases by "increment" the score of an author/genre if it exists, sets it to "increment" if it doesn't
+    private void IncrementScore(ref Dictionary<string, int> dico, string author, int increment)
     {
         if (dico.ContainsKey(author))
         {
@@ -69,7 +85,6 @@ public class UserObject : MonoBehaviour
             dico[author] = increment;
         }
     }
-
     private void IncrementScore(ref Dictionary<Genre, int> dico, Genre genre, int increment)
     {
         if (dico.ContainsKey(genre))
@@ -82,7 +97,8 @@ public class UserObject : MonoBehaviour
         }
     }
 
-    private void ShelfAuthorScore(ref Dictionary<string, int> scores, Shelf shelf, int increment) // Scans a premade shelf and increase the score of the authors it contains
+    // ShelfAuthorScore(): Scans a premade shelf and increases the score of the authors it contains
+    private void ShelfAuthorScore(ref Dictionary<string, int> scores, Shelf shelf, int increment)
     {
         IEnumerable<BookData> booksHeld = shelf.GetBooks(); // Need a method to recover the books in a shelf
 
@@ -91,7 +107,8 @@ public class UserObject : MonoBehaviour
         }
     }
 
-    private void ShelfGenreScore(ref Dictionary<Genre, int> scores, Shelf shelf, int increment) // Scans a premade shelf and increase the score of the genres it contains
+    // ShelfGenreScore(): Scans a premade shelf and increases the score of the genres it contains
+    private void ShelfGenreScore(ref Dictionary<Genre, int> scores, Shelf shelf, int increment)
     { 
         IEnumerable<BookData> booksHeld = shelf.GetBooks();
 
@@ -100,39 +117,39 @@ public class UserObject : MonoBehaviour
         }
     }
 
-    private Dictionary<string, int> GetAuthorScores() // Get the score for all the authors in a premade shelf
+    // GetAuthorScores(): Gets the scores for all the authors in a premade shelf
+    private Dictionary<string, int> GetAuthorScores()
     {
-        // Need methods to acquire each predefined shelf in a user's data
-        //Shelf toRead = Data.getToReadShelf();
-        //Shelf reading = Data.getToReadShelf();
-        //Shelf read = Data.getToReadShelf();
-        //Shelf favorite = Data.getToReadShelf();
-        Shelf toRead = null, reading = null, read = null, favorite = null;
+        Shelf toRead = getShelf(ShelfType.TOREAD);
+        Shelf reading = getShelf(ShelfType.READING);
+        Shelf read = getShelf(ShelfType.READ);
+        Shelf favorites = getShelf(ShelfType.FAVORITES);
+        //Shelf toRead = null, reading = null, read = null, favorite = null;
 
         Dictionary<string, int> authorScores = new Dictionary<string, int>();
 
         ShelfAuthorScore(ref authorScores, toRead, 1); // Having a book in the toRead shelf yields 1 point
         ShelfAuthorScore(ref authorScores, reading, 1); // Having a book in the reading shelf yields 2 point
         ShelfAuthorScore(ref authorScores, read, 1); // Having a book in the read shelf yields 2 point
-        ShelfAuthorScore(ref authorScores, favorite, 3); // Having a book in the favorite shelf yields 1 additional point
+        ShelfAuthorScore(ref authorScores, favorites, 3); // Having a book in the favorite shelf yields 1 additional point
 
         return authorScores;
     }
-
-    private Dictionary<Genre, int> GetGenreScores() // Get the score for all the genres in a premade shelf
+    // GetGenreScores(): Gets the scores for all the genres in a premade shelf
+    private Dictionary<Genre, int> GetGenreScores()
     {
-        //Shelf toRead = this.getToReadShelf();
-        //Shelf reading = this.getToReadShelf();
-        //Shelf read = this.getToReadShelf();
-        //Shelf favorite = this.getToReadShelf();
-        Shelf toRead = null, reading = null, read = null, favorite = null;
+        Shelf toRead = getShelf(ShelfType.TOREAD);
+        Shelf reading = getShelf(ShelfType.READING);
+        Shelf read = getShelf(ShelfType.READ);
+        Shelf favorites = getShelf(ShelfType.FAVORITES);
+        //Shelf toRead = null, reading = null, read = null, favorite = null;
 
         Dictionary<Genre, int> genreScores = new Dictionary<Genre, int>();
 
         ShelfGenreScore(ref genreScores, toRead, 1);
         ShelfGenreScore(ref genreScores, reading, 1);
         ShelfGenreScore(ref genreScores, read, 1);
-        ShelfGenreScore(ref genreScores, favorite, 3);
+        ShelfGenreScore(ref genreScores, favorites, 3);
 
         return genreScores;
     }
